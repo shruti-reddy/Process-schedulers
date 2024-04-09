acecdb6a24dfc07700e0a01cd6b31aed49f956fd
shruthi <g.shruthireddy14@gmail.com>
2024-04-08 22:34:00 -0500

    uploading local changes to git

---
+++ src/components/AlgorithmChooser.vue
@@ -0,0 +1,77 @@
+<script>
+import ProcessTable from "../components/ProcessTable.vue";
+export default {
+    name: 'algorithm-chooser',
+    components: {ProcessTable},
+    data() {
+        return {
+            algorithms: [
+                {
+                    title: "FCFS",
+                    description: "The first come first serve algorithm is a scheduling algorithm that schedules the processes in the order of their arrival in the ready queue.",
+                    route: "/fcfs",
+                },
+                {
+                    title: "SJF",
+                    description: "The shortest job first algorithm is a scheduling algorithm that schedules the processes in the order of their burst time.",
+                    route: "/sjf",
+                },
+                {
+                    title: "Round Robin",
+                    description: "The round robin algorithm is a scheduling algorithm that schedules the processes in the order of their arrival in the ready queue.",
+                    route: "/roundrobins",
+                },
+            ],
+            selectedAlgorithm: '',
+        }
+    },
+    methods: {
+        selectedAlgorithmChanged(algo) {
+            this.selectedAlgorithm = algo.title;
+            console.log(this.selectedAlgorithm)
+        }
+    }
+}
+</script>
+
+<template>
+    <h1><b>Select CPU shceduling algorithm</b></h1>
+    <div class="container">
+        <div class="card" v-for="algo in algorithms" :key="algo">
+            <button class="title" v-on:click="selectedAlgorithmChanged(algo)">{{ algo.title }}</button>
+            <p class="description">{{ algo.description }}</p>
+        </div>
+    </div>
+    <ProcessTable :algorithm="selectedAlgorithm"/>
+</template>
+
+<style>
+.container {
+    display: flex;
+    flex: row;
+}
+
+.title {
+    background-color: #42b883;
+    color: white;
+    border-radius: 5px;
+    border: 1px solid grey;
+    font-size: larger;
+    margin: 20px;
+    width: 150px;
+    height: 40px;
+}
+
+.title:hover + .description {
+  display: block;
+  color: rgb(43, 43, 99);
+}
+
+.button-selected {
+    background-color: blue;
+}
+
+.description {
+    display: none;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,62 @@
+<style>
+.process-container {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+.process {
+  width: 50px;
+  height: 50px;
+  background-color: blue;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  margin-right: 10px;
+}
+</style>
+
+<template>
+  <div id="app">
+    <AlgorithmChooser/>
+  </div>
+</template>
+
+<script>
+import AlgorithmChooser from "../components/AlgorithmChooser.vue";
+export default {
+  components: {AlgorithmChooser},
+  data() {
+    return {
+      processes: [
+        { name: 'P1', arrivalTime: 0, burstTime: 5 },
+        { name: 'P2', arrivalTime: 2, burstTime: 3 },
+        { name: 'P3', arrivalTime: 4, burstTime: 1 },
+        { name: 'P4', arrivalTime: 5, burstTime: 2 }
+      ],
+      executedProcesses: [],
+      showExecution: false
+    }
+
+  },
+  methods: {
+    runFCFS() {
+      this.showExecution = true;
+      let currentTime = 0;
+      for (let process of this.processes) {
+        currentTime = Math.max(currentTime, process.arrivalTime);
+        this.executedProcesses.push({
+          name: process.name,
+          arrivalTime: currentTime,
+          burstTime: process.burstTime,
+          endTime: currentTime + process.burstTime
+        });
+        currentTime += process.burstTime;
+      }
+    }
+  }
+};
+</script>
