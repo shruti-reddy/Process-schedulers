8ce371681dcf5d98e2b83ce3bb7ef096913f6186
priya <gantlapriya4@gmail.com>
2024-04-14 19:15:30 -0500

    round robin

---
+++ src/helpers/RoundRobin.js
@@ -0,0 +1,85 @@
+function calculateOutputForRR(processes, quantum) {
+    debugger;
+    // Sort processes based on arrival time
+    processes.sort((a, b) => a.arrivalTime - b.arrivalTime);
+
+    const n = processes.length;
+    // const burstRemaining = new Array(n);
+    const burstRemaining = processes.map(p => p.burstTime);
+    const waitingTime = new Array(n).fill(0);
+    const completionTime = new Array(n).fill(0);
+    const turnaroundTime = new Array(n).fill(0);
+    let outputProcesses = [];
+
+    // Initialize burst remaining array
+    // for (let i = 0; i < n; i++) {
+    //     burstRemaining[i] = processes[i].burstTime;
+    // }
+
+    let currentTime = processes[0].arrivalTime;
+    const queue = [];
+
+    while (true) {
+        let done = true;
+
+        for (let i = 0; i < n; i++) {
+            if (burstRemaining[i] > 0 && processes[i].arrivalTime <= currentTime) {
+                done = false;
+
+                if (burstRemaining[i] > quantum) {
+                    currentTime += quantum;
+                    burstRemaining[i] -= quantum;
+                } else {
+                    currentTime += burstRemaining[i];
+                    waitingTime[i] = currentTime - processes[i].burstTime - processes[i].arrivalTime;
+                    burstRemaining[i] = 0;
+                    completionTime[i] = currentTime;
+                    turnaroundTime[i] = completionTime[i] - processes[i].arrivalTime;
+                }
+
+                // Check for new arrivals
+                // for (let j = 0; j < n; j++) {
+                //     if (processes[j].arrivalTime <= currentTime && burstRemaining[j] > 0 && !queue.includes(j)) {
+                //         queue.push(j);
+                //     }
+                // }
+            }
+        }
+
+        if (done) break;
+
+        // Rotate the queue
+        // if (queue.length > 0) {
+        //     let nextProcess = queue.shift();
+        //     queue.push(nextProcess);
+        // }
+    }
+
+    processes.forEach(process => {
+        const outputProcess = { ...process };
+        outputProcesses.push(outputProcess);
+    })
+
+   
+    for (let i = 0; i < n; i++) {
+        outputProcesses[i]['waitingTime'] = waitingTime[i];
+        outputProcesses[i]['completionTime'] = completionTime[i]
+        outputProcesses[i]['turnAroundTime'] = turnaroundTime[i]
+    }
+
+   
+   
+     // Calculate total waiting time and turnaround time
+     let totalWaitingTime = waitingTime.reduce((acc, val) => acc + val, 0);
+     let totalTurnaroundTime = turnaroundTime.reduce((acc, val) => acc + val, 0);
+     
+ 
+     // Output results
+
+    
+     return outputProcesses;
+    }
+    
+    export default calculateOutputForRR;
+
+
